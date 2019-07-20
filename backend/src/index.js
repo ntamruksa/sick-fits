6a2d6067de7d6c45a8c8ab3bdd43feb8e01cf436
Niss Tamruksa <ptamruksa@gmail.com>
2019-07-20 13:13:36 +1000

    db server for yoga

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' })
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO use express middleware to handle cookies
+// TODO Use express middle to populate current user
+
+server.start(
+  {
+    cors: {
+      credential: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`server is now running on port http://localhost:${deets.port}`)
+  }
+)
