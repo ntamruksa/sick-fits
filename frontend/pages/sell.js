8e81d8a9aa08cc01f3bd7a257b487d0a889accae
Niss Tamruksa <ptamruksa@gmail.com>
2019-09-02 07:40:18 +1000

    createItem init

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
+import CreateItem from '../components/CreateItem'
 
 const Sell = () => {
   return (
-    <div>
-      <p>Sell</p>
-    </div>
+    <CreateItem />
   );
 };
 
